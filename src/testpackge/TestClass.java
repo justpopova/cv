2372e638c59016f8ba404f8fc7aead57569230cd
MxOnEinstein <treynxd@gmail.com>
2022-01-21 10:48:31 +0200

    hey hey hey

--- src/testpackge/TestClass.java
+++ src/testpackge/TestClass.java
@@ -1,5 +1,14 @@
-package testpackge;
-
-public class TestClass {
-
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        Double num = scanner.nextDouble();
+        if (num>0){
+            num = -num;
+        }
+        else {
+            num = num;
+        }
+        System.out.println(num);
+    }
 }
